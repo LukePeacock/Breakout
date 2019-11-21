6edb83a1d813ee16875690db377794e4b32f75c3
Luke Peacock <lukepeacock13@gmail.com>
2019-11-21 11:45:57 +0000

    collision detection

--- src/headers/game.hpp
+++ src/headers/game.hpp
@@ -20,6 +20,13 @@ enum GameState {
     GAME_WIN
 };
 
+// Represents the four possible (collision) directions
+enum Direction {
+    UP,
+    RIGHT,
+    DOWN,
+    LEFT
+};
 
 // Initial size of the player paddle
 const glm::vec2 PLAYER_SIZE(100, 20);
@@ -31,6 +38,11 @@ const GLfloat PLAYER_VELOCITY(500.0f);
 const glm::vec2 INITIAL_BALL_VELOCITY(100.0f, -350.0f);
 // Radius of the ball object
 const GLfloat BALL_RADIUS = 12.5f;
+
+
+// Defines a Collision typedef that represents collision data
+typedef std::tuple<GLboolean, Direction, glm::vec2> Collision; // <collision?, what direction?, difference vector center - closest point>
+
 
 // Game holds all game-related state and functionality.
 // Combines all game-related data into a single class for
@@ -53,6 +65,10 @@ public:
     void ProcessInput(GLfloat dt);
     void Update(GLfloat dt);
     void Render();
+    void DoCollisions();
+    // Reset
+    void ResetLevel();
+    void ResetPlayer();
 };
 
 #endif /* game_hpp */
