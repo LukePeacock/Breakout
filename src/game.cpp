973f69264f5808a7379bb4ff0c5a97bbba1eab89
Luke Peacock <lukepeacock13@gmail.com>
2019-11-21 11:06:24 +0000

    paddle now moves left and right

--- src/game.cpp
+++ src/game.cpp
@@ -77,7 +77,21 @@ void Game::Update(GLfloat dt)
 
 void Game::ProcessInput(GLfloat dt)
 {
-
+    if (this->State == GAME_ACTIVE)
+    {
+        GLfloat velocity = PLAYER_VELOCITY * dt;
+        // Move playerboard
+        if (this->Keys[GLFW_KEY_A])
+        {
+            if (Player->Position.x >= 0)
+                Player->Position.x -= velocity;
+        }
+        if (this->Keys[GLFW_KEY_D])
+        {
+            if (Player->Position.x <= this->Width - Player->Size.x)
+                Player->Position.x += velocity;
+        }
+    }
 }
 
 void Game::Render()
